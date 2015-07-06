b3005a9d71f053f1d2feb113e1a632119534198b
jAANUSZEK0700 <jAANUSZEK0700@gmail.com>
2015-07-06 11:37:35 +0200

    Początek projektu, podstawowy wygląd.

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-07-06 07:30:57 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-07-06T07:30:57+00:00	INFO ::1	update	Update started by user Super User (42). Old version is 3.4.1.
+2015-07-06T07:30:58+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.3/Joomla_3.4.x_to_3.4.3-Stable-Patch_Package.zip.
+2015-07-06T07:31:02+00:00	INFO ::1	update	File Joomla_3.4.x_to_3.4.3-Stable-Patch_Package.zip successfully downloaded.
+2015-07-06T07:31:03+00:00	INFO ::1	update	Starting installation of new version.
+2015-07-06T08:38:39+00:00	INFO ::1	update	Update started by user Super User (42). Old version is 3.4.1.
+2015-07-06T08:38:39+00:00	INFO ::1	update	File Joomla_3.4.x_to_3.4.3-Stable-Patch_Package.zip successfully downloaded.
+2015-07-06T08:38:40+00:00	INFO ::1	update	Starting installation of new version.
---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-07-06 09:06:36 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-07-06T09:06:36+00:00	INFO ::1	joomlafailure	Nieprawidłowa nazwa użytkownika lub hasło albo nie masz u nas jeszcze konta
